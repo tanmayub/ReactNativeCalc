7da56712fc7b92093d2a0ca3291419ce39400815
Tanmay Budukh <tanmayub26@gmail.com>
2020-03-22 21:57:45 -0400

    full functionality - calc

--- src/styles.js
+++ src/styles.js
@@ -4,35 +4,38 @@ const { StyleSheet } = React;
 export default {
 
     container: {
-        flex: 1,
+        flex:1,
         flexDirection: 'column'
     },
 
-    contHistory: {
-        flex: 0.3,
-        backgroundColor: 'red'
+    contHistory:{
+        flex: 0.35,
+        borderBottomWidth: 1,
+        borderColor: '#000',
     },
 
-    contOutput: {
-        flex: 0.2
+    contOutput:{
+        flex: 0.25,
     },
 
-    contButtons: {
-        flex: 0.5,
+    contButtons:{
+        flex: 0.4,
         backgroundColor: '#bdc3c7'
     },
 
     placeHolderOutput: {
         flex: 1,
-        backgroundColor: 'green',
+        backgroundColor: '#fff',
         justifyContent: 'center',
         alignItems: 'flex-end',
-        paddingRight: 20
+        paddingRight: 15,
+        paddingLeft: 15
     },
 
     txtDefault: {
-        color: '#fff',
+        color: '#000',
         fontFamily: 'Helvetica-Light',
         fontSize: 30
     }
+
 };
